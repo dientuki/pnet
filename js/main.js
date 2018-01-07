bedb287f18cd9c79680a43a87b0575f6ea637643
Juan 'Dientuki' Farias <dientuki@gmail.com>
2018-01-07 20:49:37 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(window).scroll(function(event){
+
+  var yOffset = window.pageYOffset;
+  var breakpoint = 50;
+  if (yOffset > breakpoint){
+
+$("header").removeClass('default');
+    $("header").addClass('active');
+    
+  }else{
+    $("header").removeClass('active');
+    $("header").addClass('default');
+  }
+
+});
+
+
+function display() {
+   $(this).children("ul").show();
+}
+
+function hide() {
+   $(this).children("ul").hide();   
+}
+
+
+$(function(){
+   $("ul.main li").hover(display,hide);
+   $("ul.main li").focus(display);
+   $("ul.main li").blur(hide);
+});
+
